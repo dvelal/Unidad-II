49c60ea54d5fa82219feba4f1ae3a39689808f1f
ejyanaca <elvisyanacayo@gmail.com>
2025-12-14 16:33:25 -0500

    Prueba de envio

---
+++ ItemVenta.h
@@ -0,0 +1,12 @@
+class ItemVenta {
+    private:
+        Producto producto;
+        int cantidad;
+        float subTotal;
+
+    public:
+        ItemVenta(Producto p, int c);
+        float calcularSubtotal();
+        Producto getProducto();
+        int getCantidad();
+};
