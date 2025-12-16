a3608735486c9cbacb0618e852bd6605633bdd10
wilfredo300 <junior.val6@gmail.com>
2025-12-16 15:49:17 -0500

    implementacion del menu, case 1,2,3

--- main.cpp
+++ main.cpp
@@ -10,15 +10,18 @@ int main(){
     do{
         std::cout<<"Escoge una opción: "<<std::endl;
         std::cout<<"1. Agregar Producto\n";
-        std::cout<<"2. Actualizar stock\n";
-        std::cout<<"3.... \n";//...
-        std::cout<<"0. Salir"<<std::endl;
+        std::cout<<"2. Mostrar productos\n";
+        std::cout<<"3. Reponer stock\n";
+        std::cout<<"4. Mostrar productos bajo minimo\n";
+        std::cout<<"0. Volver"<<std::endl;
         char opt;
         cin>>opt;
         switch(opt){
-            case '1':
+            case '1':{
+                std::cout<<"/////////Agregar Producto/////////"<<std::endl;
                 std::cout<<"Que producto quiere ingresar?"<<std::endl;;
                 std::cout<<"a. Torta"<<std::endl;
+                //otros produtos...
                 char opt2;
                 cin>>opt2;
                 switch(opt2){
@@ -28,11 +31,40 @@ int main(){
                     break;
                 }
                 break;
-            case '2':
-                //inventario.actualizarStock();
-            
+            }    
+            case '2':{
+                std::cout<<"/////////////////////////  Lista de Productos  /////////////////////////"<<std::endl;
+                inventario.mostrarProductos();
                 
             break;
+            }
+            case '3':{
+                std::cout<<"/////////////////////////  Reponer stock  /////////////////////////"<<std::endl;
+                std::string id;
+                std::cout<<"Ingresa id del producto: \n";
+                cin>>id;
+
+                Producto *p = inventario.buscarPorId(id);
+                if(!p){
+                    "El producto no existe";
+                    break;
+                }
+                p->mostrarDatos();
+                int cant;
+                std::cout<<"Cuanta[s] "<<p->getNombre()<<" va a ingresar?"<<std::endl;
+                cin>>cant;
+                inventario.actualizarStock(id,cant);
+                std::cout<<"Se agregaron correctamente: "<<cant<<" "<<p->getNombre()<<std::endl;
+                p->mostrarDatos();
+            break;
+            }
+            case '4':{
+
+                //inventario.actualizarStock();
+                
+                
+                break;
+            }
             case '0':
                 close = true;
                 break;
