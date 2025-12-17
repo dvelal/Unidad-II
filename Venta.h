9c826b1cb9053b99c5e46d1bc8be75ebb0f0f61c
ejyanaca <elvisyanacayo@gmail.com>
2025-12-16 23:17:49 -0500

    Mejora de venta.h

--- Venta.h
+++ Venta.h
@@ -1,5 +1,6 @@
 #include<string>
 #include <vector>
+#include<Producto.h>
 
 class Venta {
 private:
@@ -17,3 +18,68 @@ public:
     void mostrarVenta();
     vector<ItemVenta> getItems();
 };
+
+Venta::Venta(int id, string f, string h) {
+    idVenta = id;
+    fecha = f;
+    hora = h;
+    total = 0;
+}
+
+void Venta::agregarItem(Producto p, int cantidad) {
+    ItemVenta item(p, cantidad);
+    listItems.push_back(item);
+}
+
+void Venta::calcularTotal() {
+    total = 0;
+    for (auto &item : listItems) {
+        total = total + item.getSubtotal();
+    }
+}
+
+void Venta::mostrarVenta() {
+    cout << "Venta N°: " << idVenta << endl;
+    cout << "Fecha: " << fecha << " Hora: " << hora << endl;
+
+    for (ItemVenta &item : listItems) {
+        cout << item.getProducto().getNombre()
+             << " x" << item.getCantidad()
+             << " = " << item.getSubtotal() << endl;
+    }
+
+    cout << "TOTAL: " << total << endl;
+}
+#include<string>
+
+Venta::Venta(int id, string f, string h) {
+    idVenta = id;
+    fecha = f;
+    hora = h;
+    total = 0;
+}
+
+void Venta::agregarItem(Producto p, int cantidad) {
+    ItemVenta item(p, cantidad);
+    listItems.push_back(item);
+}
+
+void Venta::calcularTotal() {
+    total = 0;
+    for (auto &item : listItems) {
+        total = total + item.getSubtotal();
+    }
+}
+
+void Venta::mostrarVenta() {
+    cout << "Venta N°: " << idVenta << endl;
+    cout << "Fecha: " << fecha << " Hora: " << hora << endl;
+
+    for (ItemVenta &item : listItems) {
+        cout << item.getProducto().getNombre()
+             << " x" << item.getCantidad()
+             << " = " << item.getSubtotal() << endl;
+    }
+
+    cout << "TOTAL: " << total << endl;
+}
